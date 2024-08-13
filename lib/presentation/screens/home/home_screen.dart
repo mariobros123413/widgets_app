38635a40a78638d475a006220cf38c48856eb42b
mariobros123413 <j.mario18@hotmail.es>
2024-08-12 21:53:37 -0400

    rutas básicas configuradas

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_app/config/menu/menu-items.dart';
+import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -48,7 +49,12 @@ class _CustomListTile extends StatelessWidget {
       trailing: const Icon(Icons.arrow_forward_ios_outlined),
       title: Text(menuItem.title),
       subtitle: Text(menuItem.subTitle),
-      onTap: () {},
+      onTap: () {
+        // Navigator.of(context).push( // OPCION 1 MUY LARGO Y TEDIOSO, PERMITE DEEP LINKS
+        //   MaterialPageRoute(builder: (context) => const ButtonsScreen()),
+        // );
+        // Navigator.of(context).pushNamed(menuItem.link); // OPCION 2 (NO PERMITE CUSTOMIZAR DEEP LINKS)
+      },
     );
   }
 }
