f2967962c1d8eb1f228910928a8c9d7d855b9ddf
mariobros123413 <j.mario18@hotmail.es>
2024-08-19 00:03:04 -0400

    snackbars dialogs terminado

--- lib/config/menu/menu-items.dart
+++ lib/config/menu/menu-items.dart
@@ -26,4 +26,16 @@ const appMenuItems = <MenuItem>[
     link: '/cards',
     icon: Icons.credit_card,
   ),
+  MenuItem(
+    title: 'Progress Indicators',
+    subTitle: 'Generales y controlados',
+    link: '/progress',
+    icon: Icons.refresh_rounded,
+  ),
+  MenuItem(
+    title: 'Snackbars y diálogos',
+    subTitle: 'Indicadores en pantalla',
+    link: '/snackbars',
+    icon: Icons.info_outline,
+  ),
 ];
--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -1,11 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class SnackBarScreen extends StatelessWidget {
   static const name = 'snackbar_screen';
   const SnackBarScreen({super.key});
+  void showCustomSnackBar(BuildContext context) {
+    ScaffoldMessenger.of(context).clearSnackBars();
+
+    final snackBar = SnackBar(
+      content: const Text('Tienes un nuevo mensaje!'),
+      action: SnackBarAction(
+        label: 'Ok!',
+        onPressed: () {},
+      ),
+      duration: const Duration(seconds: 2),
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  void openDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible:
+          false, //No cerrar dialogo si se pulsa afuera del diálogo
+//builder se construye en tiempo de ejecución
+      builder: (context) => AlertDialog(
+        title: const Text('¿Estás seguro?'),
+        content: const Text(
+          'Laborum aliquip laborum velit est ex amet anim occaecat cillum aliqua eiusmod. Aliqua incididunt consequat esse deserunt laborum commodo nostrud consectetur ea proident aliquip exercitation excepteur. Sunt elit ex magna dolor eiusmod ullamco non ullamco. Ea proident quis eiusmod adipisicing. Reprehenderit cillum tempor fugiat tempor fugiat proident voluptate aliqua ea. Ipsum est esse mollit enim anim reprehenderit qui laboris id do mollit tempor cupidatat consequat.',
+        ),
+        actions: [
+          TextButton(
+            onPressed: () => context.pop(),
+            child: const Text('Cancelar'),
+          ),
+          FilledButton(
+            onPressed: () => context.pop(),
+            child: const Text('Aceptar'),
+          )
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Snackbars y Diálogos'),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () => showCustomSnackBar(context),
+        label: const Text('Show Snackbar'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FilledButton(
+              onPressed: () {
+                showAboutDialog(context: context);
+              },
+              child: const Text('Licencias usadas'),
+            ),
+            FilledButton.tonal(
+              onPressed: () {
+                openDialog(context);
+              },
+              child: const Text('Mostrar diálogos'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
