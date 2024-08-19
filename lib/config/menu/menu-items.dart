973977954a50078123f452e1f3f87fd695f9931c
mariobros123413 <j.mario18@hotmail.es>
2024-08-19 17:58:07 -0400

    tutorial screen added

--- lib/config/menu/menu-items.dart
+++ lib/config/menu/menu-items.dart
@@ -45,9 +45,9 @@ const appMenuItems = <MenuItem>[
     icon: Icons.check_box_outline_blank_rounded,
   ),
   MenuItem(
-    title: 'UI Controls + Tiles',
-    subTitle: 'Una serie de controles de flutter',
-    link: '/ui-controls',
-    icon: Icons.car_rental_outlined,
+    title: 'Introducción a la Aplicación',
+    subTitle: 'Pequeño tutorial introductorio',
+    link: '/tutorial',
+    icon: Icons.accessible_rounded,
   ),
 ];
